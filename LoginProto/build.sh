9b8fe6d70be242264ed3117b0bf9b4c4035bb5f9
LG <lg@LgFaceRecProject.localdomain>
2021-06-09 03:41:37 -0400

    Login Sample

---
+++ LoginProto/build.sh
@@ -0,0 +1,3 @@
+g++ Login.cpp ../Common/Protocol/LoginProtocol.cpp ../Common/Protocol/protocolLogin.pb.cc ../Common/NetworkTCP.cpp -o Login -I/usr/include -I/usr/include/opencv4/ -I../Common -L/usr/local/lib -lprotobuf -lprotobuf-lite -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_video -lopencv_videoio
+
+
