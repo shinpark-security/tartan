6eca1842bdee3aaaaa1abc6a39a8cac0a5300fad
Sungsoo Kim <kimss0418@naver.com>
2021-06-11 09:34:34 +0900

    move cert location & add tls function to common directory

--- RecvImageTCP/build.sh
+++ RecvImageTCP/build.sh
@@ -1,3 +1,3 @@
-g++ RecvImageTCP.cpp ../Common/NetworkTCP.cpp ../Common/TcpSendRecvJpeg.cpp ../Common/Protocol/protocol.pb.cc ../Common/Protocol/ProtoBuff.cpp -o RecvImageTCP -I../Common -L/usr/local/lib -I/usr/local/include/opencv4/ -lprotobuf -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_video -lopencv_videoio 
+g++ RecvImageTCP.cpp ../Common/NetworkTCP.cpp ../Common/TcpSendRecvJpeg.cpp ../Common/Protocol/protocol.pb.cc ../Common/Protocol/ProtoBuff.cpp -o RecvImageTCP -I../Common -L/usr/local/lib -I/usr/local/include/opencv4/ -lprotobuf -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_video -lopencv_videoio -lwolfssl 
 
 
