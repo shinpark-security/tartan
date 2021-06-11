8f3e354713867dd8f7749ac464b161e4a26f379c
seokwang kim <seokwang.kim@lge.com>
2021-06-11 15:03:11 +0900

    protocol buffer code gen script

--- RecvImageTCP/build.sh
+++ RecvImageTCP/build.sh
@@ -1,3 +1,3 @@
-g++ RecvImageTCP.cpp ../Common/NetworkTCP.cpp ../Common/TcpSendRecvJpeg.cpp ../Common/Protocol/protocol.pb.cc ../Common/Protocol/ProtoBuff.cpp -o RecvImageTCP -I../Common -L/usr/local/lib -I/usr/local/include/opencv4/ -lprotobuf -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_video -lopencv_videoio -lwolfssl 
+g++ RecvImageTCP.cpp ../Common/NetworkManager.cpp ../Common/Protocol/ProtocolManager.cpp ../Common/Protocol/ImageProtocol.cpp ../Common/Protocol/LoginProtocol.cpp ../Common/NetworkTCP.cpp ../Common/TcpSendRecvJpeg.cpp ../Common/Protocol/Msg/protocolImage.pb.cc ../Common/Protocol/Msg/protocolLogin.pb.cc  -o RecvImageTCP -I../Common -I../Common/Protocol -I../Common/Protocol/Msg -L/usr/local/lib -I/usr/local/include/opencv4/ -lprotobuf -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_video -lopencv_videoio -lwolfssl 
 
 
