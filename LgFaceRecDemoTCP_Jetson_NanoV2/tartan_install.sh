f5720dd2046fe2e75f80dd3be0af2892f3c9e5e9
seokwang.kim <dolnyumi@gmail.com>
2021-06-17 08:23:30 -0400

    install script

--- LgFaceRecDemoTCP_Jetson_NanoV2/tartan_install.sh
+++ LgFaceRecDemoTCP_Jetson_NanoV2/tartan_install.sh
@@ -1,16 +1,19 @@
 
 mkdir -p /usr/local/tartan/bin
-cp build/LgFaceRecDemoTCP_Jetson_NanoV2 /usr/local/tartan/bin/
-cp *.mp4 /usr/local/tartan/
-cp tartan*.db /usr/local/tartan/
-cp mtCNNModels /usr/local/tartan/ -rf
-cp facenetModels /usr/local/tartan/ -rf
-cp facenet.pb /usr/local/tartan/
+systemctl stop tartan
+install build/LgFaceRecDemoTCP_Jetson_NanoV2 /usr/local/tartan/bin/ -v
+install log.sh   /usr/local/tartan/bin/ -v
+install restart_server.sh  /usr/local/tartan/bin/ -v
+install *.mp4 /usr/local/tartan/ -v
+install tartan*.db /usr/local/tartan/
+install /usr/local/tartan/mtCNNModels -d -v
+install -D mtCNNModels/* /usr/local/tartan/mtCNNModels/  -v
+install /usr/local/tartan/facenetModels -d -v
+install -D facenetModels/* /usr/local/tartan/facenetModels/  -v
+install facenet.pb /usr/local/tartan/ -v
 cp shinpark /var/ -rf
-cp tartan.service /lib/systemd/system/
-cp log.sh   /usr/local/tartan/
-cp restart_server.sh  /usr/local/tartan/
-sudo systemctl enable tartan 
-sudo systemctl start tartan 
+install tartan.service /lib/systemd/system/ -v
+systemctl enable tartan 
+systemctl start tartan 
 
 
